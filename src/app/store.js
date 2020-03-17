4f7d984de6c83a249771d403e9a2f4f7477d6313
benjaminaudet <benjamin@audet.fr>
2020-03-17 18:11:15 +0100

    remove useless files

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import videosReducer from './Containers/videoSlice';
-import thunk from 'redux-thunk';
 
 export default configureStore({
   reducer: {
     videos: videosReducer,
-    counter: counterReducer,
   },
 });
